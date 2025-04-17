0663a8dc1f4ba4e1cc848e5777596db274fa6976
Twigman <twigman@posteo.de>
2025-04-17 17:40:33 +0200

    solved day 1

---
+++ day1/solve.py
@@ -0,0 +1,33 @@
+import numpy as np
+
+input_file = "input.txt"
+num_entries = 0
+
+# number of lines
+with open(input_file, "r") as f:
+    num_entries = sum(1 for _ in f)
+
+left_arr = np.empty(num_entries, dtype=int)
+right_arr = np.empty(num_entries, dtype=int)
+
+# lines to arrays
+with open(input_file) as f:
+    i = 0
+    for line in f:
+        arr_line = line.split('   ')
+        left_arr[i] = int(arr_line[0])
+        right_arr[i] = int(arr_line[1])
+        i += 1
+
+# sort
+left_arr_sorted = np.sort(left_arr)
+right_arr_sorted = np.sort(right_arr)
+
+# calc distance
+if len(left_arr_sorted) == len(right_arr_sorted):
+    diff = left_arr_sorted - right_arr_sorted
+    abs_diff = np.abs(diff)
+    total_distance = np.sum(abs_diff)
+    print('sum of distances: ' + str(total_distance))
+else:
+    print('error in input list')
