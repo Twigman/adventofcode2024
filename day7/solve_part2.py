a85a1f604650bce3cc9bc3ac2e58703993886848
Twigman <twigman@posteo.de>
2025-04-19 12:26:58 +0200

    added solution for day 7 part 2

---
+++ day7/solve_part2.py
@@ -0,0 +1,57 @@
+import re
+import numpy as np
+
+def load_data_from_file(filename: str) -> list:
+    with open(filename) as f:
+        data = f.readlines()
+    return data
+
+
+def extract_numbers_from(line: str) -> np.array:
+    res = re.findall(r'\d+', line)
+    return np.array(res, dtype=int)
+
+
+def generate_solution_pattern(eq: np.array) -> np.array:
+    allowed_ops = 3
+    n_pos = (len(eq) - 2)
+    if n_pos > 0:
+        n_variations = allowed_ops ** n_pos
+        values = np.arange(n_variations)[:, None]
+        powers = allowed_ops ** np.arange(n_pos)[::-1]
+        ops = (values // powers) % allowed_ops
+        return ops
+    return None
+
+
+def is_solvable(eq: np.array, ops: np.array) -> bool:
+    for op_bits in ops:
+        res = eq[1]
+        for i, bit in enumerate(op_bits):
+            if bit == 0:
+                res += eq[2 + i]
+            elif bit == 1:
+                res *= eq[2 + i]
+            else:
+                res = int(str(res) + str(eq[2 + i]))
+        if res == eq[0]:
+            return True
+    return False
+
+
+input_file = 'input.txt'
+eqs = load_data_from_file(input_file)
+sum_solved_eq = 0
+
+for line in eqs:
+    eq = extract_numbers_from(line)
+    ops = generate_solution_pattern(eq)
+
+    if ops is None:
+        continue
+
+    if is_solvable(eq, ops):
+        sum_solved_eq += eq[0]
+
+print('Part 2:')
+print(f"solution: {sum_solved_eq}")
